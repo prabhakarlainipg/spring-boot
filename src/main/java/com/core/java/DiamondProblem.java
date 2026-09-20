54c80354edc319de81d0d0fdc8ffe74ac0ee3c00
Prabhakar Laini <prabhakar.laini.pg@gmail.com>
2026-09-20 22:29:24 +0530

    default methods java8

---
+++ src/main/java/com/core/java/DiamondProblem.java
@@ -0,0 +1,26 @@
+package com.core.java;
+
+interface A {
+    default void show() {
+        System.out.println("A");
+    }
+}
+
+interface B {
+    default void show() {
+        System.out.println("B");
+    }
+}
+public class DiamondProblem implements A,B {
+
+    //super - selects its inherited default implementation.
+    @Override
+    public void show() {
+        A.super.show();
+    }
+
+    public static void main(String[] args){
+        DiamondProblem d = new DiamondProblem();
+        d.show();
+    }
+}
