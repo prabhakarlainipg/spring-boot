17e26ebffc3bf2e6c9025e0b43753a77a0e108e1
Prabhakar Laini <prabhakar.laini.pg@gmail.com>
2026-09-20 09:22:17 +0530

    @PostConstruct, @PreDestroy

--- src/main/java/com/example/learning/service/GreetingService.java
+++ src/main/java/com/example/learning/service/GreetingService.java
@@ -1,16 +1,27 @@
 package com.example.learning.service;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service
 public class GreetingService {
 
     public GreetingService() {
         System.out.println("1.Greeting Service is created");
     }
+    @PostConstruct
+    public void initialize() {
+        System.out.println("2. PostConstruct: bean initialized");
+    }
 
     public String greet(String name) {
 
         return "Hello, " +name + "! Welcome to Spring.";
+    }
+
+    @PreDestroy
+    public void cleanup() {
+        System.out.println("3. PreDestroy: bean being destroyed");
     }
 }
